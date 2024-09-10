152202fe1a9f0b3790f4c6e87c44aa7182e2b3e9
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-10 15:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+ let inputs = document.querySelectorAll("input")
+    let first = inputs[0].value;
+    let second = inputs[1].value
+    // console.log(first)
+    alert(first+second)
 }
